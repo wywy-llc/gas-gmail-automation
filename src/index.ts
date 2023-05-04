cd61da585b45b6fa4cd3e8c66df4d321c1101bf7
yuki_fujisawa <g031y148yuki@gmail.com>
2023-05-04 17:40:40 +0900

    メール転送ロジックの実装

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 /**
- * Copyright 2023 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * メールを転送する。
  */
-console.log('Hello, World!');
+function sendEmails() {
+  const message = searchMessages_('subject:AED');
+  Logger.log(`メッセージ件数: ${message.length}`);
+}
+/**
+ * メールを検索する。
+ */
+function searchMessages_(query: string): GoogleAppsScript.Gmail.GmailMessage[] {
+  const threads = GmailApp.search(query);
+  Logger.log(`スレッド件数: ${threads.length}`);
+  if (threads.length === 0) {
+    return [];
+  }
+  return threads.map(thread => thread.getMessages()).flat();
+}
